66ceab2c873fc26cf365b4b688462aee75220662
Gabriel Canarin Salazar <86989100+GabrielCanarin@users.noreply.github.com>
2024-06-15 15:44:23 -0300

    Terraform para iniciar ADLS

---
+++ adls-azure-main/variables.tf
@@ -0,0 +1,12 @@
+
+# Lista de variaveis utilizadas nos arquivos de terraform
+variable "resource_group_name" {
+  default = "Cafeteria_engDados"
+
+
+
+}
+
+variable "location" {
+  default = "westus"
+}
